611e30388821735a289addfa18ff9a69d118a18a
Danny Ramirez <dcr1999jr@gmail.com>
2022-09-02 12:36:24 -0700

    ASSESSMENT

---
+++ profile.js
@@ -0,0 +1,15 @@
+let colorBtn = document.querySelector('#color')
+let placeBtn = document.querySelector('#place')
+let ritualBtn = document.querySelector('#ritual')
+
+colorBtn.addEventListener('click', () => {
+    console.log("Green")
+})
+
+placeBtn.addEventListener('click', () => {
+    console.log("San Diego")
+})
+
+ritualBtn.addEventListener('click', () => {
+  console.log("Video games after class")
+})
