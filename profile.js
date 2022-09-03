f2a77bf4bb8184338cbaac975f61c1c81aced12a
Danny Ramirez <dcr1999jr@gmail.com>
2022-09-02 19:09:45 -0700

    ASSESSMENT

--- profile.js
+++ profile.js
@@ -1,7 +1,7 @@
 let colorBtn = document.querySelector('#color')
 let placeBtn = document.querySelector('#place')
 let ritualBtn = document.querySelector('#ritual')
-let movieForm = document.querySelector('#form') 
+
 const favColor = () => {
     alert('My favorite color is Green')
 }
@@ -14,10 +14,6 @@ const favRitual = () => {
     alert('My favorite ritual is videogames after class')
 }
 
-const submitForm = (event) => {
-    event.preventDefault()
-alert('Im watching what i want')
-}
 
 colorBtn.addEventListener('click', favColor)
   
@@ -27,5 +23,5 @@ placeBtn.addEventListener('click', favPlace)
 
 ritualBtn.addEventListener('click', favRitual)
 
-movieForm.addEventListener('submit', submitForm)
+
 
